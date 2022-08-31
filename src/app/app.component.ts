203565dc55dae056b96b6de364869f1087bc3b13
James W <51789232+FalsePozitive@users.noreply.github.com>
2022-08-31 16:09:48 +0200

    View Chld decrorator base functionality
    view child decorator allows a programmatic reference. allows direct access at component level instead of template like ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
 import {Course} from './model/course';
 
 @Component({
@@ -26,9 +27,19 @@ rate = 0.67;
 course= COURSES[0];
 
 startDate = new Date(2000, 0, 1) ;
+
+
+// view child decorator allows a programmatic reference. allows direct access at component level instead of template like ng-template
+@ViewChild(CourseCardComponent) //need to name the component you need access to
+card: CourseCardComponent //set var
+
+
+
 // method logs object and appends course object
 onCourseSelected(course:Course) {
+  console.log(this.card);
   console.log("App component is clicked", course);
 }
 
 }
+
