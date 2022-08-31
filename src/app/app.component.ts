3cb4f529f0628f8eade79ce55e9adf974c1430b3
James W <51789232+FalsePozitive@users.noreply.github.com>
2022-08-31 15:27:45 +0200

    Built-in Pipes
    learned how to implement pipes for date, currency, percentage, strings, and keyvalue when used on an ngFor loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,15 @@ export class AppComponent {
 // rxjsCourse = COURSES[1];
 // // bracketed number is the key to objects position in array found db-data.ts
 // ngrxCourse = COURSES[2];
+title = COURSES[0].description;
 
+price = 9.993453465;
+
+rate = 0.67;
+
+course= COURSES[0];
+
+startDate = new Date(2000, 0, 1) ;
 // method logs object and appends course object
 onCourseSelected(course:Course) {
   console.log("App component is clicked", course);
